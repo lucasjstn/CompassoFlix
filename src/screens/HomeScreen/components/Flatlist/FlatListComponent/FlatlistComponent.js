d267fda1df0c794a489604e0d0d1c275573cc9ec
Charles Junior <selrahcjunior@hotmail.com>
2022-08-03 07:53:45 -0300

    Adição do click no Card do filme que leva pra tela de detalhes

--- src/screens/HomeScreen/components/Flatlist/FlatListComponent/FlatlistComponent.js
+++ src/screens/HomeScreen/components/Flatlist/FlatListComponent/FlatlistComponent.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import {TouchableOpacity, Text, View, Image, StyleSheet } from 'react-native';
+import {TouchableOpacity, Text, View, Image} from 'react-native';
 import  Icon  from 'react-native-vector-icons/AntDesign';
 import styles from './style';
-
+import { useNavigation } from "@react-navigation/native";
 
 export default function FilmesCP({image, rating}){
+    const navigation = useNavigation()
     return(
         <View style={styles.conteinerImage}>
-            <TouchableOpacity>
+            <TouchableOpacity onPress={() => navigation.navigate('Details')}>
                 <Image source={image} style={styles.Img}/>
             </TouchableOpacity>
             <Icon name='star' color={'red'} size={10} style={{top:5}}/>
