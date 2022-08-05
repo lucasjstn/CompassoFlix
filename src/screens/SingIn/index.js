bcc7e0142e6bb20e67dafae4260aee051e65f8d2
lucasjstn <lucas7justino@gmail.com>
2022-08-05 12:01:45 -0300

    alterando as dimensões da logo pra responder com tamanho da tela

--- src/screens/SingIn/index.js
+++ src/screens/SingIn/index.js
@@ -1,33 +1,65 @@
-
-import React from "react";
-import { TextInput, Image, Text, View, TouchableOpacity, KeyboardAvoidingView } from "react-native";
-import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import React from 'react';
+import {
+  TextInput,
+  Image,
+  Text,
+  View,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+} from 'react-native';
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
 import EvilIcons from 'react-native-vector-icons/EvilIcons';
 import styles from '../SingIn/styles';
-import Banner from "./components/Banner/index";
+import Banner from './components/Banner/index';
+import {height, width} from './consts';
 const SignIn = () => {
-
-    return(
-<KeyboardAvoidingView style={styles.container} behavior="position" enabled={true}>
+  return (
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior="position"
+      enabled={true}>
       <Banner />
-      <Image style={styles.logo} width={231} height={228} source={require('../../../assets/logo.png')}/>
+
+      <Image
+        style={styles.logo}
+        width={0.54 * width}
+        height={0.25 * height}
+        source={require('../../../assets/logo.png')}
+      />
+
       <Text style={styles.textlogin}>Login</Text>
       <Text style={styles.textentrar}>Entre na sua conta para continuar.</Text>
-      <View style={{alignItems: 'center', justifyContent: 'center', }}>
-      <TextInput placeholderTextColor='#ffffff' placeholder="e-mail" style={styles.emailinput}></TextInput>
-      <EvilIcons style={{position: 'absolute', left: 100}} name='user' size={20} color={'#ffffff80'}/>
+
+      <View style={{alignItems: 'center', justifyContent: 'center'}}>
+        <TextInput
+          placeholderTextColor="#ffffff"
+          placeholder="e-mail"
+          style={styles.emailinput}></TextInput>
+        <EvilIcons
+          style={{position: 'absolute', left: 100}}
+          name="user"
+          size={20}
+          color={'#ffffff80'}
+        />
       </View>
-      <View style={{alignItems: 'center', justifyContent: 'center', }}>
-      <TextInput placeholderTextColor='#ffffff' placeholder="senha" secureTextEntry={true} style={styles.emailinput}></TextInput>
-      <EvilIcons style={{position: 'absolute', left: 100}} name='lock' size={20} color={'#ffffff80'}/>
+      <View style={{alignItems: 'center', justifyContent: 'center'}}>
+        <TextInput
+          placeholderTextColor="#ffffff"
+          placeholder="senha"
+          secureTextEntry={true}
+          style={styles.emailinput}></TextInput>
+        <EvilIcons
+          style={{position: 'absolute', left: 100}}
+          name="lock"
+          size={20}
+          color={'#ffffff80'}
+        />
       </View>
       <TouchableOpacity style={styles.botaologin}>
         <Text style={styles.botaotexto}>Entrar</Text>
       </TouchableOpacity>
-</KeyboardAvoidingView>
-
-    
-    )
-}
+    </KeyboardAvoidingView>
+  );
+};
 
 export default SignIn;
