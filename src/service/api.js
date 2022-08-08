7cf5b8686846ee75c2c49109f04fada6960f08ab
gabrielSanovis <9novis9@gmail.com>
2022-08-08 00:30:02 -0300

    removendo os params da apiImage, se viu inutil

--- src/service/api.js
+++ src/service/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
+    
     baseURL: 'https://api.themoviedb.org/3',
     params: {
         api_key: '6483151dfec6e35c01b29e2d1b76788f'
@@ -8,7 +9,4 @@ export const api = axios.create({
 })
 export const apiImage = axios.create({
     baseURL: 'https://image.tmdb.org/t/p/',
-    params: {
-        api_key: '6483151dfec6e35c01b29e2d1b76788f'
-    }
 })
