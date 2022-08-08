17adcb3bde4b4369d160780a50025a320aaa6f84
gabrielSanovis <9novis9@gmail.com>
2022-08-08 19:17:34 -0300

    Import do modal da imagem

--- src/screens/FilmsDetails/components/Header/index.js
+++ src/screens/FilmsDetails/components/Header/index.js
@@ -1,15 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import styles from './style'
 import { apiImage } from '../../../../service/api';
 import transformInAround from './transformInAround';
+import ImgWindow from './imgWindow';
 const baseUrl = apiImage.defaults.baseURL;
 
 export default FilmsDetails = ({ title, runtime, backdrop_path, poster_path, popularity, vote_average, release_date, director, min, directorBy, directorDefault }) => {
 
     const [information, setInformation] = useState(0);
     const [press, setPress] = useState(false);
+    const [modalVisible, setModalVisible] = useState(false)
 
     const searchPeople = (array) => {
         return array?.filter(obj => obj?.job === 'Director')
@@ -23,64 +25,84 @@ export default FilmsDetails = ({ title, runtime, backdrop_path, poster_path, pop
     }, [title])
 
     return (
-        <View>
-            <Image
-                source={{ uri: `${baseUrl}/original${backdrop_path}` }}
+        <>
 
-                style={styles.poster} />
-            <Image
-                source={{ uri: `${baseUrl}/w500${poster_path}` }}
-                style={styles.frontCover}
+            <ImgWindow
+                poster_path={poster_path}
+                visible={modalVisible}
+                setVisible={setModalVisible}
             />
 
-            <View style={styles.mainWrapper}>
+            <View>
 
-                <View style={styles.mainTextWrapper}>
+                <Image
+                    source={{ uri: `${baseUrl}/original${backdrop_path}` }}
 
-                    <View style={{ flexDirection: 'row', alignItems: 'flex-end', justifyContent: 'center' }}>
+                    style={styles.poster} />
+
+                <TouchableOpacity 
+                    style={{
+                        position: 'absolute'
+                    }}
+                    activeOpacity={0.6}
+                    onPress={() => setModalVisible(true)}
+                >
+                    <Image
+                        source={{ uri: `${baseUrl}/w500${poster_path}` }}
+                        style={styles.frontCover}
+                    />
+                </TouchableOpacity>
 
 
-                        <View style={[styles.popUpWrapper, { opacity: information }]}>
+                <View style={styles.mainWrapper}>
 
-                            <Text style={styles.popUpTitle}>Pressione para mostrar o título completo</Text>
-                            <View style={styles.bottomPopUp}></View>
+                    <View style={styles.mainTextWrapper}>
+
+                        <View style={{ flexDirection: 'row', alignItems: 'flex-end', justifyContent: 'center' }}>
+
+
+                            <View style={[styles.popUpWrapper, { opacity: information }]}>
+
+                                <Text style={styles.popUpTitle}>Pressione para mostrar o título completo</Text>
+                                <View style={styles.bottomPopUp}></View>
+                            </View>
+
+                            <View style={[styles.popUpWrapper, { opacity: press ? 1 : 0 }]}>
+                                <Text style={styles.popUpTitle}>{title}</Text>
+                                <View style={styles.bottomPopUp}></View>
+                            </View>
+
+                            <Text
+                                onLongPress={() => setPress(true)}
+                                onPressOut={() => setPress(false)}
+                                style={styles.frontCoverTitle}>{
+                                    title?.length < 14
+                                        ? title
+                                        : `${title?.slice(0, 14)}...`
+                                }<Text style={styles.frontCoverLaunch}> {release_date?.slice(0, 4)}</Text></Text>
                         </View>
 
-                        <View style={[styles.popUpWrapper, { opacity: press ? 1 : 0 }]}>
-                            <Text style={styles.popUpTitle}>{title}</Text>
-                            <View style={styles.bottomPopUp}></View>
-                        </View>
-                        
-                        <Text
-                            onLongPress={() => setPress(true)}
-                            onPressOut={() => setPress(false)}
-                            style={styles.frontCoverTitle}>{
-                                title?.length < 14
-                                    ? title
-                                    : `${title?.slice(0, 14)}...`
-                            }<Text style={styles.frontCoverLaunch}> {release_date?.slice(0, 4)}</Text></Text>
+                        <Text style={styles.frontCoverMin}>{runtime} {min}</Text>
                     </View>
 
-                    <Text style={styles.frontCoverMin}>{runtime} {min}</Text>
-                </View>
+                    <Text style={styles.directorText}>{directorBy} <Text style={{ fontWeight: '700' }}>{searchPeople(director)?.[0]?.name || directorDefault}</Text></Text>
 
-                <Text style={styles.directorText}>{directorBy} <Text style={{ fontWeight: '700' }}>{searchPeople(director)?.[0]?.name || directorDefault}</Text></Text>
+                    <View style={styles.ratingWrapper}>
 
-                <View style={styles.ratingWrapper}>
+                        <Text style={styles.rating}>{vote_average?.toFixed(1)}/10</Text>
 
-                    <Text style={styles.rating}>{vote_average?.toFixed(1)}/10</Text>
+                        <View style={styles.votesWrapper}>
+                            <Icon
+                                name='favorite'
+                                size={30}
+                                color='red'
+                            />
 
-                    <View style={styles.votesWrapper}>
-                        <Icon
-                            name='favorite'
-                            size={30}
-                            color='red'
-                        />
-
-                        <Text style={styles.votesText}>{transformInAround(popularity)}</Text>
+                            <Text style={styles.votesText}>{transformInAround(popularity)}</Text>
+                        </View>
                     </View>
                 </View>
             </View>
-        </View>
+        </>
     );
 }
