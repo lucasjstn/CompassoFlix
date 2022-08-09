7d192176d6f5164f92a9f6c08274c85e5583ec24
Charles Junior <selrahcjunior@hotmail.com>
2022-08-09 17:37:59 -0300

    Adição Scroll Infinito

--- src/screens/HomeScreen/components/Flatlist/FlatlistF.js
+++ src/screens/HomeScreen/components/Flatlist/FlatlistF.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { FlatList, View } from "react-native";
+import { ActivityIndicator, FlatList, View } from "react-native";
 import { useNavigation } from '@react-navigation/native';
 import FilmesCP from "./FlatListComponent/FlatlistComponent";
 import styles from './style'
@@ -10,11 +10,14 @@ export default function FlatFilmes(){
     const navigation = useNavigation()
     const numColumns = 4;
     const [movies, SetMovies] = useState('')
+    const [pagina, SetPagina] = useState(1)
     
     const getMovies = async () =>{
-        await api.get('/movie/popular?&language=pt-BR&page=1')
+        await api.get(`/movie/popular?&language=pt-BR&page=${pagina}`)
     .then((res) => {
-        SetMovies(res.data.results)
+        const current = res.data.results
+        SetMovies(prev => [...prev, ...current])
+        SetPagina(prev => prev + 1)
     }).catch(err => console.log(`Opa, erro nisso aqui ${err}`))
     }
 
@@ -42,6 +45,9 @@ export default function FlatFilmes(){
                 renderItem={renderItem}
                 keyExtractor={item => item.id}
                 numColumns={numColumns}
+                ListFooterComponent={<ActivityIndicator color={'red'}/>}
+                onEndReached={getMovies}
+                onEndReachedThreshold={0.01}
                 />
             </View>
         </View>
