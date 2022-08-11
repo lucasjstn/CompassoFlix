4c507a37b14fac32c2a8202e398778bf0dc2f561
gabrielSanovis <9novis9@gmail.com>
2022-08-11 14:44:03 -0300

    corrigindo bug do Math.trunc

--- src/screens/FilmsDetails/components/Header/transformInAround.js
+++ src/screens/FilmsDetails/components/Header/transformInAround.js
@@ -1,16 +1,17 @@
 export default function transformInAround(number) {
-    Math?.trunc(number)
-    let string = String(number)
-    if (number < 1000) {
-        return number
-    } else if (number < 10000) {
+    const numberTrunc = Math?.trunc(number)
+    console.log(numberTrunc)
+    let string = String(numberTrunc)
+    if (numberTrunc < 1000) {
+        return numberTrunc
+    } else if (numberTrunc < 10000) {
         string = string.slice(0, 2)
         return `${string[1] > 0 ? string.replace(string.slice(0, 1), `${string.slice(0, 1)}.`) : string.slice(0, 1)}k`
-    } else if (number < 1000000) {
-        return `${number.toString().slice(0, number < 100000 ? 2 : 3)}k`
-    } else if (number < 100000000) {
-        return `${number?.toString().slice(0, number < 10000000 ? 1 : 2)}kk`
+    } else if (numberTrunc < 1000000) {
+        return `${numberTrunc.toString().slice(0, numberTrunc < 100000 ? 2 : 3)}k`
+    } else if (numberTrunc < 100000000) {
+        return `${numberTrunc?.toString().slice(0, numberTrunc < 10000000 ? 1 : 2)}kk`
     } else {
-        return `${number?.toString().slice(0, 3)}M+`
+        return `${numberTrunc?.toString().slice(0, 3)}M+`
     }
 }
