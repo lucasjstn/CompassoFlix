26e1f1b9dac3b96b9fd8dd839baff2af7e9aa7cb
gabrielSanovis <9novis9@gmail.com>
2022-08-11 16:42:55 -0300

    remoção do params da apiImage

--- src/service/api.js
+++ src/service/api.js
@@ -7,8 +7,5 @@ export const api = axios.create({
   },
 });
 export const apiImage = axios.create({
-  baseURL: 'https://image.tmdb.org/t/p/',
-  params: {
-    api_key: '6483151dfec6e35c01b29e2d1b76788f',
-  },
+  baseURL: 'https://image.tmdb.org/t/p/'
 });
