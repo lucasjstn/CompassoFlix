2d97a4602758344997883c9ca335d6afe2b1c90e
gabrielSanovis <9novis9@gmail.com>
2022-08-11 14:43:28 -0300

    formatação do código

--- src/screens/FilmsDetails/components/Header/ImgWindow/index.js
+++ src/screens/FilmsDetails/components/Header/ImgWindow/index.js
@@ -1,36 +1,33 @@
 import React from 'react';
-import { View, Image, Modal, TouchableOpacity } from 'react-native';
-import Icon from 'react-native-vector-icons/AntDesign'
-import { apiImage } from '../../../../../service/api';
+import {View, Modal, TouchableOpacity} from 'react-native';
+import Icon from 'react-native-vector-icons/AntDesign';
+import {apiImage} from '../../../../../service/api';
 import styles from './style';
 import WatchScreen from '../../../../WatchScreen';
 const baseUrl = apiImage.defaults.baseURL;
 
-export default function ImgWindow({ id, visible, setVisible }) {
+export default function ImgWindow({id, visible, setVisible}) {
+  console.log('renderizou modal');
 
-    return (
-        <Modal
-            animationType="fade"
-            transparent={true}
-            visible={visible}
-        >
-                
-            <View style={styles.centeredView}>
-                    <TouchableOpacity
-                        onPress={() => setVisible(false)}
-                        activeOpacity={0.7}
-                        style={styles.btn}
-                    >
-                        <Icon name='closecircle' color='#f5f5f5' size={55} style={styles.icon}/>
-                    </TouchableOpacity>
+  return (
+    <Modal animationType="fade" transparent={true} visible={visible}>
+      <View style={styles.centeredView}>
+        <TouchableOpacity
+          onPress={setVisible}
+          activeOpacity={0.7}
+          style={styles.btn}>
+          <Icon
+            name="closecircle"
+            color="#f5f5f5"
+            size={55}
+            style={styles.icon}
+          />
+        </TouchableOpacity>
 
-                <View style={styles.modalView}>
-                    <WatchScreen id={id} />
-                </View>
-            </View>
-        </Modal>
-
-
-    );
-
+        <View style={styles.modalView}>
+          <WatchScreen id={id} />
+        </View>
+      </View>
+    </Modal>
+  );
 }
