e550b5e6107e432ac614f769b06b39ff66c38ffd
gabrielSanovis <9novis9@gmail.com>
2022-08-11 14:42:32 -0300

    Adicionando o loading

--- src/screens/FilmsDetails/index.js
+++ src/screens/FilmsDetails/index.js
@@ -1,25 +1,32 @@
 import React from 'react';
-import { SafeAreaView } from 'react-native';
+import {SafeAreaView} from 'react-native';
 import Header from './components/Header';
 import Synopsis from './components/Synopsis';
 import Cast from './components/Cast';
 import BtnGoBack from '../../components/BtnGoBack';
 import styles from './style';
 import mockFilmsDetails from '../../mocks/filmsDetails';
+import Loading from '../../components/Loading';
 import apiGets from './apiGets';
 
-export default FilmsDetails = ({ route, navigation }) => {
+export default FilmsDetails = ({route, navigation}) => {
+  const {id} = route.params;
+  const {data: dataDetails} = apiGets(`/movie/${id}?&language=pt-BR`);
+  const {data: dataCredits} = apiGets(`/movie/${id}/credits?&language=pt-BR`);
 
-    const { id } = route.params;
-    const { data: dataDetails } = apiGets(`/movie/${id}?&language=pt-BR`)
-    const { data: dataCredits } = apiGets(`/movie/${id}/credits?&language=pt-BR`)
-
-    return (
-        <SafeAreaView style={styles.container} >
-            <BtnGoBack nav={navigation} />
-            <Header {...dataDetails} director={dataCredits?.crew} {...mockFilmsDetails.header} id={id} />
-            <Synopsis {...dataDetails} {...mockFilmsDetails.synopsis} />
-            <Cast cast={dataCredits?.cast} {...mockFilmsDetails.cast} />
-        </SafeAreaView>
-    );
-}
+  return !!dataCredits ? (
+    <SafeAreaView style={styles.container}>
+      <BtnGoBack nav={navigation} />
+      <Header
+        {...dataDetails}
+        director={dataCredits?.crew}
+        {...mockFilmsDetails.header}
+        id={id}
+      />
+      <Synopsis {...dataDetails} {...mockFilmsDetails.synopsis} />
+      <Cast cast={dataCredits?.cast} {...mockFilmsDetails.cast} />
+    </SafeAreaView>
+  ) : (
+    <Loading fullScreen={false} />
+  );
+};
