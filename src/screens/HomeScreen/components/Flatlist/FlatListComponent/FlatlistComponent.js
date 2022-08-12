1e579704cede1197e948ee76a608d5cc1a665e46
gabrielSanovis <9novis9@gmail.com>
2022-08-12 01:15:09 -0300

    correção do bug da flatlist

--- src/screens/HomeScreen/components/Flatlist/FlatListComponent/FlatlistComponent.js
+++ src/screens/HomeScreen/components/Flatlist/FlatListComponent/FlatlistComponent.js
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, {memo} from 'react';
 import {TouchableOpacity, Text, View, Image} from 'react-native';
-import  Icon  from 'react-native-vector-icons/AntDesign';
+import Icon from 'react-native-vector-icons/AntDesign';
 import styles from './style';
-import { apiImage } from '../../../../../service/api';
+import {apiImage} from '../../../../../service/api';
+import {useNavigation} from '@react-navigation/native';
 
-export default function FilmesCP({image, rating, onPress}){
-    return(
-        <View style={styles.conteinerImage}>
-            <TouchableOpacity onPress={onPress}>
-                <Image source={{uri: `${apiImage.defaults.baseURL}/original${image}` }} style={styles.Img}/>
-            </TouchableOpacity>
-            <Icon name='star' color={'red'} size={10} style={styles.icon}/>
-            <Text style={styles.note}>{`${rating}/10`}</Text>
-        </View>
-    )
-}
+const FilmesCP = memo(({vote_average, poster_path, id}) => {
+  const navigation = useNavigation();
+  return (
+    <View style={styles.conteinerImage}>
+      <TouchableOpacity
+        onPress={() => {
+          navigation.navigate('Details', {id: id});
+        }}>
+        <Image
+          source={{uri: `${apiImage.defaults.baseURL}/original${poster_path}`}}
+          style={styles.Img}
+        />
+      </TouchableOpacity>
+      <Icon name="star" color={'red'} size={10} style={styles.icon} />
+      <Text style={styles.note}>{`${vote_average}/10`}</Text>
+    </View>
+  );
+});
 
+export default FilmesCP;
