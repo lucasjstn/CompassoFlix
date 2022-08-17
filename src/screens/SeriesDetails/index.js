aba21b9b06da9de02e5b15ad925aaa05101d45f7
gabrielSanovis <9novis9@gmail.com>
2022-08-17 02:35:58 -0300

    As novas telas

---
+++ src/screens/SeriesDetails/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+const SeriesDetails = () => {
+  return (
+    <SafeAreaView>
+      <Text>Tela Séries Detalhes</Text>
+    </SafeAreaView>
+  );
+};
+
+export default SeriesDetails;
---
+++ src/screens/SeriesScreen/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+const SeriesScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>Tela Séries</Text>
+    </SafeAreaView>
+  );
+};
+
+export default SeriesScreen;
