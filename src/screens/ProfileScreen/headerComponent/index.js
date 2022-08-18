d59e11e880bd85d87c5ddb56ce29b8b63ea71470
Charles Junior <selrahcjunior@hotmail.com>
2022-08-18 07:26:50 -0300

    Adição de um loading temporario e da requisição das avaliações

--- src/screens/ProfileScreen/headerComponent/index.js
+++ src/screens/ProfileScreen/headerComponent/index.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from "react";
-import { View, StyleSheet, Image } from "react-native";
+import { View, ActivityIndicator} from "react-native";
 import { TextBold, TextRegular, TextSemiBold } from "../../../components/Text";
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { api, apiImage } from "../../../service/api";
@@ -10,13 +10,17 @@ export default function SupProfile(){
 
     const [avatar, setAvatar] = useState('')
     const [nomes, setNomes] = useState({name: undefined, username: 'none'});
-    const [id, setId] = useState('')
+    const [ratedseries, setRatedSerie] = useState()
+    const [ratedmovie, setRatedMovie] = useState()
+    const [isLoad, setIsLoad] = useState(true);
 
       const getUsuario = async () => {
         try {
           const value = await AsyncStorage?.getItem('@session');
           if (value !== null) {
             getUser(value);
+            getRtSeries(value);
+            getRtMovies(value);
           }
         } catch (err) {
           console.log('erro aqui ó: ' + err);
@@ -28,12 +32,26 @@ export default function SupProfile(){
         try {
           setNomes({name: res?.data?.name, username: res?.data?.username});
           setAvatar(res.data.avatar.tmdb.avatar_path)
-          setId(res.data.id)
         } catch (err) {
           console.log('erro aqui ó: ' + err);
-        }
+        } 
       };
-      
+
+      const getRtSeries = async sessionId => {
+        const res = await api.get(`/account/account_id/rated/tv?&session_id=${sessionId}`);
+        try {setRatedSerie(res.data.total_results)} 
+        catch (err) {console.log('erro aqui ó: ' + err)}
+      };
+
+      const getRtMovies = async sessionId => {
+        await api
+          .get(`/account/account_id/rated/movies?&session_id=${sessionId}`)
+          .then(res => {
+            setRatedMovie(res.data.total_results);
+          })
+          .catch(err => {(`deu erro aqui ô: ${err}`)})
+          .finally(() => setIsLoad(false))
+      };
 
       useEffect(() => {
         getUsuario();
@@ -54,8 +72,11 @@ export default function SupProfile(){
             </View>
             <TextBold style={styles.nome}>{nomes.name || nomes.username}</TextBold>
             <View style={styles.ratedConteiner}>
-                <TextBold style={styles.rate}>30</TextBold>
-                <TextRegular style={styles.textRated}>Avaliações</TextRegular>
+                { isLoad ? (<ActivityIndicator color={'red'}/>)
+                :<View style={{flexDirection: 'row', justifyContent: 'center'}}>
+                    <TextBold style={styles.rate}>{ratedseries + ratedmovie}</TextBold>
+                    <TextRegular style={styles.textRated}>Avaliações</TextRegular>
+                </View>}
             </View>
         </View>
     )
