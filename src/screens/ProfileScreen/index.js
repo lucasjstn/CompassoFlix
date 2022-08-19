722e567ca5a5841d1da17b4f05be693079ab5bd5
Charles Junior <selrahcjunior@hotmail.com>
2022-08-19 16:29:27 -0300

    Conclusão da tela profile

--- src/screens/ProfileScreen/index.js
+++ src/screens/ProfileScreen/index.js
@@ -1,33 +1,51 @@
-import React, { useState } from 'react';
-import {SafeAreaView, View} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {SafeAreaView, View, TouchableOpacity} from 'react-native';
 import TopFiveMovies from './components';
 import styles from './style';
 import getMovies from './getMovies';
 import SupProfile from './headerComponent';
-import Chooses from './tabComponent'
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const ProfileScreen = () => {
-  const [whatis, setWhatis] = useState('movies')
+  const [isserie, setIsserie] = useState(false)
+  const [ses, setSes] = useState(false)
+  const [focused, setIsFocused] = useState(false)
+
   const {data: filmsFavorite, isLoad: favoriteLoad} = getMovies(
-    `/account/13768649/favorite/${whatis}?&session_id=`,
+    `/account/13768649/favorite/movies?&session_id=`,
   );
   const {data: filmsRated, isLoad: ratedLoad} = getMovies(
-    `/account/13768649/rated/${whatis}?&session_id=`,
+    `/account/13768649/rated/movies?&session_id=`,
+  );
+
+  const {data: seriesFavorite} = getMovies(
+    `/account/13768649/favorite/tv?&session_id=`,
+  );
+  const {data: seriesRated} = getMovies(
+    `/account/13768649/rated/tv?&session_id=`,
   );
   return (
     <SafeAreaView style={styles.container}>
-      <Chooses/>
       <SupProfile/>
+        <TouchableOpacity style={styles.serieIconLocation} onPress={() => {setIsserie(true), setSes(true), setIsFocused(true)}}>
+          <Icon name='television-play' style={[styles.iconSeries, {color: focused ? 'white' : null}]} size={30}/>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.filmesIconLocation} onPress={() => {setIsserie(false), setSes(false), setIsFocused(false)}}>
+          <Icon name='popcorn' style={[styles.iconFilmes, {color: focused ? null : 'white'}]} size={30}/>
+        </TouchableOpacity>
+      <View style={styles.lineUp}></View>
+      <View style={styles.lineDown}></View>
+      <View style={styles.lineMid}></View>
       <TopFiveMovies
-        moviesList={filmsFavorite?.results}
-        isSerie={false}
+        moviesList={ses ? seriesFavorite?.results : filmsFavorite?.results}
+        isSerie={isserie}
         isLoad={favoriteLoad}
       />
       <View style={styles.line} />
       <TopFiveMovies
-        moviesList={filmsRated?.results}
+        moviesList={ses ? seriesRated?.results : filmsRated?.results}
         isRated={true}
-        isSerie={false}
+        isSerie={isserie}
         isLoad={ratedLoad}
       />
     </SafeAreaView>
--- src/screens/ProfileScreen/style.js
+++ src/screens/ProfileScreen/style.js
@@ -10,6 +10,38 @@ const styles = StyleSheet.create({
     borderColor: 'rgba(255, 255, 255, 0.4)',
     top: 379
   },
+  lineUp:{
+    borderBottomWidth: 1,
+    borderColor: 'rgba(255, 255, 255, 0.4)',
+    top: 319
+  },
+  lineDown:{
+    borderBottomWidth: 1,
+    borderColor: 'rgba(255, 255, 255, 0.4)',
+    top: 379
+  },
+  lineMid:{
+    borderBottomWidth: 1,
+    width: 61,
+    borderColor: 'rgba(255, 255, 255, 0.4)',
+    top: 348,
+    transform: [{rotate: '90deg'}],
+    left: 165
+  },
+  iconFilmes:{
+    position:'absolute',
+  },
+  iconSeries:{
+    position:'absolute'
+  },
+  filmesIconLocation:{
+    top: 335,
+    left: 84
+  },
+  serieIconLocation:{
+    top: 335,
+    left: 267
+  }
 });
 
 export default styles;
