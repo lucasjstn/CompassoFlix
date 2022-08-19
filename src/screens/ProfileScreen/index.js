19b020e3b0c1912bdd89ebacbe58901dbe47205b
gabrielSanovis <9novis9@gmail.com>
2022-08-19 12:50:44 -0300

    adicionei um parametro  como padrão o sessionId

--- src/screens/ProfileScreen/index.js
+++ src/screens/ProfileScreen/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import {SafeAreaView, View} from 'react-native';
 import TopFiveMovies from './components';
 import styles from './style';
-import getMovies from './getMovies';
+import getMovies from '../FilmsDetails/apiGets';
 
 const ProfileScreen = () => {
   const {data: filmsFavorite, isLoad: favoriteLoad} = getMovies(
-    `/account/13768649/favorite/movies?&session_id=`,
+    `/account/13768649/favorite/movies?&`,
   );
   const {data: filmsRated, isLoad: ratedLoad} = getMovies(
-    `/account/13768649/rated/movies?&session_id=`,
+    `/account/13768649/rated/movies?&`,
   );
 
   return (
