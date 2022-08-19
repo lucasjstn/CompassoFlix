ff44c2e7b657a7dfba03dea5232ec71ce2a052fe
gabrielSanovis <9novis9@gmail.com>
2022-08-19 12:49:54 -0300

    está integrado com a api o favoritar

--- src/components/Favorite/index.js
+++ src/components/Favorite/index.js
@@ -2,35 +2,53 @@ import React, {useState, useRef, useEffect} from 'react';
 import {TouchableOpacity} from 'react-native';
 import styles from './style';
 import LottieView from 'lottie-react-native';
-import getMovies from '../../screens/ProfileScreen/getMovies';
+import getMovies from '../../screens/FilmsDetails/apiGets';
+import {api} from '../../service/api';
 
 export default function Favorite({style, id}) {
-    const animation = useRef(null);
-    const firstRender = useRef(true);
+  const animation = useRef(null);
+  const firstRender = useRef(true);
+  const [markFav, setMarkFav] = useState(false);
 
+  const {data: favorite} = getMovies(`/movie/${id}/account_states?&`);
 
-    const {data: favorite} = getMovies(`/movie/${id}/account_states?&session_id=`);
+  const markOrUnmark = async () => {
+    await api
+      .post('/account/ACCOUNT_ID/favorite?&', {
+        media_type: 'movie',
+        media_id: id,
+        favorite: !markFav,
+      })
+      .then(() => {
+        setMarkFav(!markFav);
+      }).catch(err => {throw new Error('deu erro aqui ô ' + err)})
+  };
 
-    useEffect(() => {
-        if(favorite?.favorite !== undefined) {
-            if(firstRender.current) {
-                if(favorite?.favorite) {
-                    animation.current.play(61, 61);
-                }else {
-                    animation.current.play(18, 18);
-                }
-                firstRender.current = false;
-            }else if(favorite?.favorite) {
-                animation.current.play(10, 61);
-    
-            }else {
-                animation.current.play(18,18);
-            }
+  useEffect(() => {
+    if (favorite?.favorite !== undefined) {
+      if (firstRender.current) {
+        setMarkFav(favorite?.favorite);
+        if (favorite?.favorite) {
+          animation.current.play(61, 61);
+        } else {
+          animation.current.play(18, 18);
         }
-    }, [favorite?.favorite])
+        firstRender.current = false;
+      } else if (markFav) {
+        animation.current.play(10, 61);
+      } else {
+        animation.current.play(18, 18);
+      }
+    }
+  }, [favorite?.favorite, markFav]);
 
   return (
-    <TouchableOpacity style={style || styles.container} activeOpacity={0.7}>
+    <TouchableOpacity
+      style={style || styles.container}
+      activeOpacity={0.7}
+      onPress={() => {
+        markOrUnmark();
+      }}>
       <LottieView
         source={require('../../../assets/lottieAnimates/StartAnimated.json')}
         loop={false}
