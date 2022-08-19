c87fe4787306483b62d19812305b17cf6cdf98a0
Charles Junior <selrahcjunior@hotmail.com>
2022-08-18 21:17:33 -0300

    Adição de um useState pra identificar se é filme ou série e chamada da tab

--- src/screens/ProfileScreen/index.js
+++ src/screens/ProfileScreen/index.js
@@ -1,20 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {SafeAreaView, View} from 'react-native';
 import TopFiveMovies from './components';
 import styles from './style';
 import getMovies from './getMovies';
 import SupProfile from './headerComponent';
+import Chooses from './tabComponent'
 
 const ProfileScreen = () => {
+  const [whatis, setWhatis] = useState('movies')
   const {data: filmsFavorite, isLoad: favoriteLoad} = getMovies(
-    `/account/13768649/favorite/movies?&session_id=`,
+    `/account/13768649/favorite/${whatis}?&session_id=`,
   );
   const {data: filmsRated, isLoad: ratedLoad} = getMovies(
-    `/account/13768649/rated/movies?&session_id=`,
+    `/account/13768649/rated/${whatis}?&session_id=`,
   );
-    console.log(ProfileScreen)
   return (
     <SafeAreaView style={styles.container}>
+      <Chooses/>
       <SupProfile/>
       <TopFiveMovies
         moviesList={filmsFavorite?.results}
