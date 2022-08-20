5b3cab3c4c590acd09bf0a4061b2fbef77255476
lucasjstn <lucas7justino@gmail.com>
2022-08-20 15:37:31 -0300

    adicionando funcionalidade pra generalizar mais o uso do btnGoBack

--- src/screens/ProfileScreen/components/index.js
+++ src/screens/ProfileScreen/components/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import {View, Image} from 'react-native';
-import {TextSemiBold} from '../../../components/Text';
+import React, {useState} from 'react';
+import {View, Image, TouchableOpacity, Modal} from 'react-native';
+import {TextRegular, TextSemiBold} from '../../../components/Text';
 import styles from './style';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import {apiImage} from '../../../service/api';
 import Loading from '../../../components/Loading';
+import BtnGoBack from '../../../components/BtnGoBack';
 const baseUrl = apiImage.defaults.baseURL;
 
 export default function TopFiveMovies({
@@ -13,45 +14,65 @@ export default function TopFiveMovies({
   isRated = false,
   isLoad,
 }) {
+  const [toggleUserFavorites, setToggleUserFavorites] = useState(false);
+
+  const toggle = () => {
+    return setToggleUserFavorites(!toggleUserFavorites);
+  };
+
   return (
-    <View style={styles.container}>
-      <View style={styles.topTextsWrapper}>
-        <TextSemiBold style={styles.title}>
-          {isRated
-            ? `Avaliações de ${isSerie ? 'séries' : 'filmes'} recentes`
-            : `${isSerie ? 'Séries' : 'Filmes'} Favorit${
-                isSerie ? 'a' : 'o'
-              }s`}{' '}
-          de John
-        </TextSemiBold>
-        <TextSemiBold style={styles.seeAll}>Ver tudo</TextSemiBold>
-      </View>
-      <View style={styles.listMovies}>
-        {isLoad ? (
-          <Loading style={styles.load} />
-        ) : (
-          moviesList?.slice(0, 5)?.map((item, index) => {
-            return (
-              <View key={index} style={styles.moviesWrapper}>
-                <Image
-                  source={{uri: `${baseUrl}/w185${item.poster_path}`}}
-                  style={styles.imgWrapper}
-                />
-                <View
-                  style={[
-                    styles.ratedWrapper,
-                    {display: isRated ? 'flex' : 'none'},
-                  ]}>
-                  <Icon name="star" color={'red'} size={13} />
-                  <TextSemiBold style={styles.rated}>
-                    {Math.round(item.rating)}/10
-                  </TextSemiBold>
+    <>
+      <Modal
+        visible={toggleUserFavorites}
+        onRequestClose={() => setToggleUserFavorites(!toggleUserFavorites)}>
+        <View style={styles.userFavoritesContainer}>
+          <BtnGoBack modal={toggle} />
+          <TextRegular>Eu sou um modal foda</TextRegular>
+        </View>
+      </Modal>
+      <View style={styles.container}>
+        <View style={styles.topTextsWrapper}>
+          <TextSemiBold style={styles.title}>
+            {isRated
+              ? `Avaliações de ${isSerie ? 'séries' : 'filmes'} recentes`
+              : `${isSerie ? 'Séries' : 'Filmes'} Favorit${
+                  isSerie ? 'a' : 'o'
+                }s`}{' '}
+            de John
+          </TextSemiBold>
+          <TouchableOpacity
+            style={styles.seeAllButton}
+            onPress={() => setToggleUserFavorites(true)}>
+            <TextSemiBold style={styles.seeAll}>Ver tudo</TextSemiBold>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.listMovies}>
+          {isLoad ? (
+            <Loading style={styles.load} />
+          ) : (
+            moviesList?.slice(0, 5)?.map((item, index) => {
+              return (
+                <View key={index} style={styles.moviesWrapper}>
+                  <Image
+                    source={{uri: `${baseUrl}/w185${item.poster_path}`}}
+                    style={styles.imgWrapper}
+                  />
+                  <View
+                    style={[
+                      styles.ratedWrapper,
+                      {display: isRated ? 'flex' : 'none'},
+                    ]}>
+                    <Icon name="star" color={'red'} size={13} />
+                    <TextSemiBold style={styles.rated}>
+                      {Math.round(item.rating)}/10
+                    </TextSemiBold>
+                  </View>
                 </View>
-              </View>
-            );
-          })
-        )}
+              );
+            })
+          )}
+        </View>
       </View>
-    </View>
+    </>
   );
 }
--- src/screens/ProfileScreen/components/style.js
+++ src/screens/ProfileScreen/components/style.js
@@ -10,54 +10,64 @@ function relativeWidth(pixel) {
 }
 
 const styles = StyleSheet.create({
-    container: {
-        width: '93%',
-        alignSelf:'center',
-        marginVertical: relativeHeight(16)
-    },
-    topTextsWrapper: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'baseline'
-    },
-    title: {
-        color:'white',
-        fontSize: 12,
-        lineHeight: 14
-    },
-    seeAll: {
-        color:'#E9A6A6',
-        fontSize: 10,
-        lineHeight: 12
-    },
-    listMovies: {
-        flexDirection: 'row',
-        justifyContent: 'center',
-        marginTop: relativeHeight(12)
-    },
-    load: {
-        width: 100,
-        height:100
-    },
-    moviesWrapper: {
-        paddingHorizontal: relativeWidth(4)
-    },
-    imgWrapper: {
-        width: relativeWidth(62),
-        height: relativeHeight(93),
-        borderRadius:10
-    },
-    ratedWrapper: {
-        flexDirection: 'row',
-        alignItems:'center',
-        paddingTop: relativeHeight(6),
-    },
-    rated: {
-        color:'white',
-        fontSize:8,
-        lineHeight: 11,
-        marginLeft: relativeWidth(4)
-    }
-})
+  userFavoritesContainer: {
+    backgroundColor: 'black',
+    flex: 1,
+  },
+  seeAllButton: {
+    width: relativeWidth(55),
+    height: relativeHeight(20),
+    alignItems: 'center',
+  },
+  container: {
+    width: '93%',
+    alignSelf: 'center',
+    marginVertical: relativeHeight(16),
+  },
+  topTextsWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'baseline',
+  },
+  title: {
+    color: 'white',
+    fontSize: 12,
+    lineHeight: 14,
+  },
+  seeAll: {
+    color: '#E9A6A6',
+    fontSize: 10,
+    lineHeight: 12,
+    // left: 20
+  },
+  listMovies: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginTop: relativeHeight(12),
+  },
+  load: {
+    width: 100,
+    height: 100,
+  },
+  moviesWrapper: {
+    paddingHorizontal: relativeWidth(4),
+  },
+  imgWrapper: {
+    width: relativeWidth(62),
+    height: relativeHeight(93),
+    borderRadius: 10,
+  },
+  ratedWrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingTop: relativeHeight(6),
+  },
+  rated: {
+    color: 'white',
+    fontSize: 8,
+    lineHeight: 11,
+    marginLeft: relativeWidth(4),
+  },
+});
 
 export default styles;
