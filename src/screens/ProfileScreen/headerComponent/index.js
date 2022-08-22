38f0d8becf50921c1cd25428bad81b59a4849398
Charles Junior <selrahcjunior@hotmail.com>
2022-08-22 13:36:06 -0300

    Adicionando o modal em um arquivo separado

--- src/screens/ProfileScreen/headerComponent/index.js
+++ src/screens/ProfileScreen/headerComponent/index.js
@@ -6,11 +6,9 @@ import { api, apiImage } from "../../../service/api";
 import { Avatar } from '@react-native-material/core'
 import styles from "./style";
 import  Icon  from "react-native-vector-icons/Ionicons";
-import { useContext } from "react";
-import { AuthContext } from "../../../context/AuthContext";
+import LeaveMdl from "./leavemodalComponent";
 
 export default function SupProfile(){
-    const {isLogged, setIsLogged} = useContext(AuthContext);
     const [avatar, setAvatar] = useState('')
     const [nomes, setNomes] = useState({name: undefined, username: 'none'});
     const [ratedseries, setRatedSerie] = useState()
@@ -71,26 +69,7 @@ export default function SupProfile(){
 
     return(
         <View style={styles.conteiner}>
-            <Modal 
-            animationType="fade"
-            transparent={true}
-            visible={modalActive}
-            onRequestClose={() => setModalActive(false)}>
-              <View style={styles.leaveModal}>
-                  <TextBold style={styles.atencaoModal}>Atenção</TextBold>
-                  <TextRegular style={styles.djsModal}>Deseja mesmo sair?</TextRegular>
-                  <Pressable style={styles.cancelarModal} onPress={() => setModalActive(false)}>
-                    <TextBold style={styles.cancelartxt}>CANCELAR</TextBold>
-                  </Pressable>
-                  <Pressable style={styles.sairModal} 
-                  onPress={async () => {
-                    await AsyncStorage.removeItem('@token');
-                    setIsLogged(false);
-                  }}>
-                    <TextBold style={styles.sairtxt}>SIM</TextBold>
-                  </Pressable>
-              </View>
-            </Modal>
+            <LeaveMdl modalActive={modalActive} setModalActive={setModalActive}/>
             <TouchableOpacity style={styles.leavebuttom} onPress={() => setModalActive(true)}>
               <Icon name="md-exit-outline" style={styles.iconleave}/>
               <TextRegular style={styles.leavetxt}>Sair</TextRegular>
---
+++ src/screens/ProfileScreen/headerComponent/leavemodalComponent/style.js
@@ -0,0 +1,68 @@
+import { StyleSheet } from "react-native";
+
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    leaveModal:{
+        width: 327,
+        height: 166,
+        top: 300,
+        backgroundColor: '#FFFFFF',
+        borderRadius: 25,
+        position: 'relative',
+        left: 30,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    atencaoModal:{
+        color: 'black',
+        position: 'absolute',
+        fontSize: 14,
+        left: 32,
+        top: 30
+    },
+    djsModal:{
+        color: '#8E8E8E',
+        position: 'absolute',
+        top: 68,
+        left: 32
+    },
+    cancelarModal:{
+        position: 'absolute',
+        alignItems: 'center',
+        textAlign: 'center',
+        left: 63,
+        top: 123,
+        borderRadius: 5,
+        backgroundColor: 'black'
+    },
+    cancelartxt:{
+        fontSize: 10,
+        color: 'white',
+        marginBottom: 5,
+        marginTop: 5,
+        marginRight: 15,
+        marginLeft: 15
+    },
+    sairModal:{
+        position: 'absolute',
+        alignItems: 'center',
+        textAlign: 'center',
+        left: 169,
+        top: 123,
+        borderRadius: 5,
+        backgroundColor: 'white',
+        borderColor: 'black',
+        borderWidth: 1
+    },
+    sairtxt:{
+        fontSize: 10,
+        color: 'black',
+        marginBottom: 3,
+        marginTop: 5,
+        marginRight: 32,
+        marginLeft: 32
+    }
+})
+
+export default styles
