c0486afb5c6f088889d6f995de64f939d140dd56
gabrielSanovis <9novis9@gmail.com>
2022-08-24 21:43:51 -0300

    refatoração do código

--- src/screens/FilmsDetails/index.js
+++ src/screens/FilmsDetails/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {SafeAreaView, View} from 'react-native';
-import Header from './components/Header';
-import Synopsis from './components/Synopsis';
+import HeaderDetails from '../../components/HeaderDetails';
+import Synopsis from '../../components/Synopsis';
 import Cast from './components/Cast';
 import BtnGoBack from '../../components/BtnGoBack';
 import styles from './style';
@@ -14,6 +14,7 @@ export default FilmsDetails = ({route, navigation}) => {
   const {id} = route.params;
   const {data: dataDetails} = apiGets(`/movie/${id}?&language=pt-BR`);
   const {data: dataCredits} = apiGets(`/movie/${id}/credits?&language=pt-BR`);
+  
   return !!dataCredits ? (
     <SafeAreaView style={styles.container}>
       <View
@@ -21,13 +22,14 @@ export default FilmsDetails = ({route, navigation}) => {
         <BtnGoBack nav={navigation} />
         <Favorite id={id}/>
       </View>
-      <Header
+      <HeaderDetails
         {...dataDetails}
         director={dataCredits?.crew}
         {...mockFilmsDetails.header}
         id={id}
+        isSerie={false}
       />
-      <Synopsis {...dataDetails} {...mockFilmsDetails.synopsis} />
+      <Synopsis {...dataDetails} />
       <Cast cast={dataCredits?.cast} {...mockFilmsDetails.cast} />
     </SafeAreaView>
   ) : (
