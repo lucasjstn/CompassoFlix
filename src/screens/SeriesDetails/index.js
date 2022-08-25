779a8178195379bf5b6dc9a5774679ae6334cb05
gabrielSanovis <9novis9@gmail.com>
2022-08-24 21:45:00 -0300

    Criação da tela para detalhes das séries

--- src/screens/SeriesDetails/index.js
+++ src/screens/SeriesDetails/index.js
@@ -1,11 +1,40 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {SafeAreaView, View} from 'react-native';
+import HeaderDetails from '../../components/HeaderDetails';
+import Synopsis from '../../components/Synopsis';
+import BtnGoBack from '../../components/BtnGoBack';
+import styles from './style';
+import mockSeriesDetails from '../../mocks/seriesDetails';
+import Loading from '../../components/Loading';
+import apiGets from '../FilmsDetails/apiGets';
+import Favorite from '../../components/Favorite';
 
-const SeriesDetails = () => {
-  return (
-    <SafeAreaView>
-      <Text>Tela Séries Detalhes</Text>
+const id = '80020';
+
+const SeriesDetails = ({route, navigation}) => {
+  const {data: dataDetails} = apiGets(`/tv/${id}?&language=pt-BR`);
+
+  return !!dataDetails ? (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.btnWrapper}>
+        <BtnGoBack nav={navigation} />
+        <Favorite id={id} />
+      </View>
+      <HeaderDetails
+        {...dataDetails}
+        backdrop_path={dataDetails?.backdrop_path}
+        poster_path={dataDetails?.poster_path}
+        title={dataDetails?.name}
+        release_date={dataDetails?.first_air_date}
+        director={dataDetails?.created_by[0]}
+        {...mockSeriesDetails.header}
+        id={id}
+        isSerie={true}
+      />
+      <Synopsis {...dataDetails} />
     </SafeAreaView>
+  ) : (
+    <Loading />
   );
 };
 
