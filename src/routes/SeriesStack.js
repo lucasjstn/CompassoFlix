f5d9d90722bec346b9175baf2a2e110907b9bf53
Charles Junior <selrahcjunior@hotmail.com>
2022-08-25 11:50:09 -0300

    Arrumando o conflito na seriesStack

--- src/routes/SeriesStack.js
+++ src/routes/SeriesStack.js
@@ -1,19 +1,25 @@
-import React from 'react';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import SeriesScreen from '../screens/SeriesScreen';
-import SeriesDetails from '../screens/SeriesDetails';
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import SeriesScreen from "../screens/SeriesScreen";
+import SeriesDetails from "../screens/SeriesDetails";
 
 const TelaStack = createNativeStackNavigator();
 
 export default function SeriesStack() {
-  return (
-    <TelaStack.Navigator
-      screenOptions={{
-        headerShown: false,
-      }}
-      initialRouteName={'FlatFilmes'}>
-      <TelaStack.Screen name="FlatFilmes" component={SeriesScreen} />
-      <TelaStack.Screen name="Details" component={SeriesDetails} />
-    </TelaStack.Navigator>
-  );
+    return (
+        <TelaStack.Navigator
+            screenOptions={{
+                headerShown: false,
+            }}
+        >
+            <TelaStack.Screen 
+                name="HomeSeries" 
+                component={SeriesScreen}
+            />
+            <TelaStack.Screen 
+                name="SeriesDetails" 
+                component={SeriesDetails}
+            />
+        </TelaStack.Navigator>
+    )
 }
