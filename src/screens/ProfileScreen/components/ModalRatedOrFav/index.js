6bd2c10c654951c50e8bca92fceeff51c0107f79
gabrielSanovis <9novis9@gmail.com>
2022-08-26 20:42:35 -0300

    Separando o modal do componente TopFiveFilms

---
+++ src/screens/ProfileScreen/components/ModalRatedOrFav/index.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import RatingStarAndAverage from '../../../../components/ContentList/FlatlistComponent/RatingStarAndAverage';
+import BtnGoBack from '../../../../components/BtnGoBack';
+import {Modal, View, ScrollView, TouchableOpacity, Image} from 'react-native';
+import {TextBold} from '../../../../components/Text';
+import styles from './style';
+
+export default function ModalRatedOrFav({
+  toggleUserFavorites,
+  toggle,
+  favoriteMovies,
+  name,
+  username,
+  moviesList,
+  isRated,
+  baseUrl,
+  setToggleUserFavorites
+}) {
+  return (
+    <Modal
+      visible={toggleUserFavorites}
+      onRequestClose={() => setToggleUserFavorites(!toggleUserFavorites)}>
+      <View style={{flex: 1, backgroundColor: 'black'}}>
+        <ScrollView>
+          <TouchableOpacity style={styles.touchableWrapper}></TouchableOpacity>
+
+          <BtnGoBack modal={toggle} />
+          <View
+            key={'greetingContainer'}
+            style={
+              favoriteMovies
+                ? styles.greetingContainer
+                : styles.greetingContainerRated
+            }>
+            <TextBold style={styles.greetingText}>
+              {favoriteMovies
+                ? [
+                    'Filmes favoritos de  ',
+                    <TextBold
+                      key={'greeting'}
+                      style={styles.greetingTextUserName}>
+                      {name || username}
+                    </TextBold>,
+                    <TextBold key={'favoriteRate'} style={styles.greetingText}>
+                      !
+                    </TextBold>,
+                  ]
+                : [
+                    'Avaliações de filmes recentes de ',
+                    <TextBold
+                      key={'greetingRate'}
+                      style={styles.greetingTextUserName}>
+                      {name || username}
+                    </TextBold>,
+                    <TextBold
+                      key={'rateExclamatin'}
+                      style={styles.greetingText}>
+                      !
+                    </TextBold>,
+                  ]}
+            </TextBold>
+          </View>
+          <View style={[styles.favoriteMoviesWrapper]}>
+            {moviesList?.map((item, index) => (
+              <TouchableOpacity
+                style={null}
+                key={index}
+                onPress={() => {
+                  navigation.navigate('Details', {id: moviesList[index]?.id});
+                }}>
+                <Image
+                  source={{uri: `${baseUrl}/w185${item.poster_path}`}}
+                  style={styles.favoriteImageWrapper}
+                />
+                {isRated ? (
+                  <RatingStarAndAverage
+                    vote_average={Number(item.rating) === 10 ? 10 : item.rating}
+                  />
+                ) : null}
+              </TouchableOpacity>
+            ))}
+          </View>
+        </ScrollView>
+      </View>
+    </Modal>
+  );
+}
--- src/screens/ProfileScreen/components/index.js
+++ src/screens/ProfileScreen/components/index.js
@@ -1,13 +1,13 @@
 import React, {useState} from 'react';
-import {View, Image, TouchableOpacity, Modal, ScrollView} from 'react-native';
-import {TextBold, TextSemiBold} from '../../../components/Text';
+import {View, Image, TouchableOpacity} from 'react-native';
+import {TextSemiBold} from '../../../components/Text';
 import styles from './style';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import {apiImage} from '../../../service/api';
 import Loading from '../../../components/Loading';
-import BtnGoBack from '../../../components/BtnGoBack';
 import {useNavigation} from '@react-navigation/native';
-import RatingStarAndAverage from '../../../components/ContentList/FlatlistComponent/RatingStarAndAverage';
+
+import ModalRatedOrFav from './ModalRatedOrFav';
 const baseUrl = apiImage.defaults.baseURL;
 export default function TopFiveMovies({
   moviesList,
@@ -26,77 +26,17 @@ export default function TopFiveMovies({
 
   return (
     <>
-      <Modal
-        visible={toggleUserFavorites}
-        onRequestClose={() => setToggleUserFavorites(!toggleUserFavorites)}>
-        <View style={{flex: 1, backgroundColor: 'black'}}>
-          <ScrollView>
-            <TouchableOpacity
-              style={styles.touchableWrapper}></TouchableOpacity>
-
-            <BtnGoBack modal={toggle} />
-            <View
-              key={'greetingContainer'}
-              style={
-                favoriteMovies
-                  ? styles.greetingContainer
-                  : styles.greetingContainerRated
-              }>
-              <TextBold style={styles.greetingText}>
-                {favoriteMovies
-                  ? [
-                      'Filmes favoritos de  ',
-                      <TextBold
-                        key={'greeting'}
-                        style={styles.greetingTextUserName}>
-                        {name || username}
-                      </TextBold>,
-                      <TextBold
-                        key={'favoriteRate'}
-                        style={styles.greetingText}>
-                        !
-                      </TextBold>,
-                    ]
-                  : [
-                      'Avaliações de filmes recentes de ',
-                      <TextBold
-                        key={'greetingRate'}
-                        style={styles.greetingTextUserName}>
-                        {name || username}
-                      </TextBold>,
-                      <TextBold
-                        key={'rateExclamatin'}
-                        style={styles.greetingText}>
-                        !
-                      </TextBold>,
-                    ]}
-              </TextBold>
-            </View>
-            <View style={[styles.favoriteMoviesWrapper]}>
-              {moviesList?.map((item, index) => (
-                <TouchableOpacity
-                  style={null}
-                  key={index}
-                  onPress={() => {
-                    navigation.navigate('Details', {id: moviesList[index]?.id});
-                  }}>
-                  <Image
-                    source={{uri: `${baseUrl}/w185${item.poster_path}`}}
-                    style={styles.favoriteImageWrapper}
-                  />
-                  {isRated ? (
-                    <RatingStarAndAverage
-                      vote_average={
-                        Number(item.rating) === 10 ? 10 : item.rating
-                      }
-                    />
-                  ) : null}
-                </TouchableOpacity>
-              ))}
-            </View>
-          </ScrollView>
-        </View>
-      </Modal>
+      <ModalRatedOrFav
+        favoriteMovies={favoriteMovies}
+        isRated={isRated}
+        moviesList={moviesList}
+        name={name}
+        toggle={toggle}
+        toggleUserFavorites={toggleUserFavorites}
+        username={username}
+        baseUrl={baseUrl}
+        setToggleUserFavorites={setToggleUserFavorites}
+      />
 
       <View style={styles.container}>
         <View style={styles.topTextsWrapper}>
