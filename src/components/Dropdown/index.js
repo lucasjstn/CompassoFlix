22d6662bd451a9ee004deddd9d1ac7d50defe82c
yslanny Mesquita <jamillecosta904@gmail.com>
2022-08-26 20:49:57 -0300

    Adicionando animação na lista de episódios

--- src/components/Dropdown/index.js
+++ src/components/Dropdown/index.js
@@ -1,19 +1,25 @@
 import React, {useState} from 'react';
-import {View, TouchableOpacity, Animated, Easing} from 'react-native';
+import {View, TouchableOpacity, Animated} from 'react-native';
 import {TextBold, TextRegular} from '../Text';
 import styles from './style';
 import getMovies from '../../screens/FilmsDetails/apiGets';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
+
 export default function ({id, path, name}) {
   const [drop, setDrop] = useState(false);
+  const [listEp, setListEp] = useState(new Animated.Value(0));
 
-  const {data: episodes} = getMovies(`/tv/${id}/season/${path}?`);
+  Animated.spring(listEp, {
+    toValue: 0,
+    duration: 300,
+    useNativeDriver: false,
+  }).start();
 
   const icon = new Animated.Value(0);
-  Animated.timing(icon, {
+  Animated.spring(icon, {
     toValue: 1,
     duration: 300,
-    easing: Easing.ease,
     useNativeDriver: false,
   }).start();
 
@@ -21,18 +27,21 @@ export default function ({id, path, name}) {
     transform: [
       {
         rotate: icon.interpolate({
-          inputRange: [0, 1],
-          outputRange: drop ? ['0deg', '180deg'] : ['180deg', '0deg'],
+        inputRange: [0, 1],
+        outputRange: drop ? ['0deg', '180deg'] : ['180deg', '0deg'],
         })
       }
     ]
   }
+  const {data: episodes} = getMovies(`/tv/${id}/season/${path}?`);
+
   return (
     <View style={styles.container}>
       <TouchableOpacity
         activeOpacity={0.8}
         onPress={() => {
           setDrop(!drop);
+          setListEp(new Animated.Value(-50));
         }}
         style={styles.btnTemp}>
         <View style={styles.titleWrapper}>
@@ -54,24 +63,27 @@ export default function ({id, path, name}) {
           {backgroundColor: drop ? '#E9A6A6' : 'rgba(255, 255, 255, 0.5)'},
         ]}
       />
-      {drop &&
+      {drop && 
         episodes?.episodes.map((item, index) => {
+          
           return (
-            <View key={index} style={styles.cardEp}>
-              <TextBold style={{color: '#FFFFFF', marginLeft: 13}}>
-                T
-                {item.season_number < 10
-                  ? `0${item.season_number}`
-                  : item.season_number}{' '}
-                | E
-                {item.episode_number < 10
-                  ? `0${item.episode_number}`
-                  : item.episode_number}
-              </TextBold>
-              <TextRegular numberOfLines={1} style={styles.overview}>
-                {item?.overview}
-              </TextRegular>
-            </View>
+            <Animated.View style={{top: listEp}}>
+              <View key={index} style={styles.cardEp}>
+                <TextBold style={{color: '#FFFFFF', marginLeft: 13}}>
+                  T
+                  {item.season_number < 10
+                    ? `0${item.season_number}`
+                    : item.season_number}{' '}
+                  | E
+                  {item.episode_number < 10
+                    ? `0${item.episode_number}`
+                    : item.episode_number}
+                </TextBold>
+                <TextRegular numberOfLines={1} style={styles.overview}>
+                  {item?.overview}
+                </TextRegular>
+              </View>
+            </Animated.View>
           );
         })}
     </View>
