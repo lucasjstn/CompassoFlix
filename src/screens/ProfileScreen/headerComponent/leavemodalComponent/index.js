bd5b50ae7d62aac53958ee7183e28bd972943735
gabrielSanovis <9novis9@gmail.com>
2022-08-26 23:04:02 -0300

    refatorando os estilos

--- src/screens/ProfileScreen/headerComponent/leavemodalComponent/index.js
+++ src/screens/ProfileScreen/headerComponent/leavemodalComponent/index.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import {Modal} from 'react-native';
-import styles from '../style';
+import styles from './style';
 import {View, Pressable} from 'react-native';
 import {TextBold, TextRegular} from '../../../../components/Text';
 import AsyncStorage from '@react-native-async-storage/async-storage';
@@ -15,66 +15,31 @@ export default function LeaveMdl({modalActive, setModalActive}) {
       animationType="fade"
       transparent={true}
       visible={modalActive}
-      style={{backgroundColor: 'blue'}}
       onRequestClose={() => setModalActive(false)}>
-      <View
-        style={{
-          backgroundColor: 'white',
-          width: '80%',
-          alignSelf: 'center',
-          height: 166,
-          marginTop: '74%',
-          borderRadius: 25,
-          padding: 30,
-        }}>
-        <TextBold
-          style={{
-            color: 'black',
-            marginBottom: 17,
-            fontSize: 14,
-            lineHeigth: 21,
-          }}>
-          Atenção
-        </TextBold>
-        <TextRegular
-          style={{
-            color: '#8E8E8E',
-            marginBottom: 35,
-            fontSize: 13,
-            lineHeigth: 20,
-          }}>
-          Deseja mesmo sair?
-        </TextRegular>
-        <View style={{flexDirection: 'row', justifyContent: 'space-evenly'}}>
+      <View style={styles.container}>
+        <TextBold style={styles.atention}>Atenção</TextBold>
+        <TextRegular style={styles.warning}>Deseja mesmo sair?</TextRegular>
+        <View style={styles.btnWrapper}>
           <Pressable
-            style={{
-              width: 84,
-              backgroundColor: 'black',
-              borderRadius: 5,
-              alignItems: 'center',
-              justifyContent:'center',
-              paddingVertical:4
-            }}
+            style={styles.btnCancel}
             onPress={() => setModalActive(false)}>
-            <TextBold style={{color: 'white', fontSize: 10, lineHeigth: 15}}>
-              CANCELAR
-            </TextBold>
+            <TextBold style={styles.btnText}>CANCELAR</TextBold>
           </Pressable>
           <Pressable
-            style={{
-              width: 84,
-              backgroundColor: 'white',
-              borderWidth: 1,
-              borderRadius: 5,
-              alignItems: 'center',
-              justifyContent:'center',
-              paddingVertical:4
-            }}
+            style={[
+              styles.btnCancel,
+              {
+                backgroundColor: 'white',
+                borderWidth: 1,
+              },
+            ]}
             onPress={async () => {
               await AsyncStorage.removeItem('@token');
               setIsLogged(false);
             }}>
-            <TextBold style={{color: 'black', fontSize: 10, lineHeigth: 15}}>SIM</TextBold>
+            <TextBold style={[styles.btnText, {color:'black'}]}>
+              SIM
+            </TextBold>
           </Pressable>
         </View>
       </View>
