2f9d72ee1c63d10e3e22d1bb66a30264ffb5e5d5
gabrielSanovis <9novis9@gmail.com>
2022-09-02 22:02:13 -0300

    Conclusão funcional da tela de lista

--- src/screens/ListScreen/index.js
+++ src/screens/ListScreen/index.js
@@ -1,24 +1,103 @@
-import React from 'react';
-import {SafeAreaView} from 'react-native';
-import {TextBold} from '../../components/Text';
+import React, {useState, useEffect, useContext} from 'react';
+import {SafeAreaView, FlatList, TouchableOpacity, View} from 'react-native';
+import {TextBold, TextRegular} from '../../components/Text';
 import BtnGoBack from '../../components/BtnGoBack';
 import styles from './style';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import CreateList from './components/CreateList';
+import {api} from '../../service/api';
+import {AuthContext} from '../../context/AuthContext';
 
 export default function ListScreen() {
+  const {listUpdate, setListUpdate} = useContext(AuthContext);
+  const [modalActive, setModalActive] = useState(false);
+  const [meta, setMeta] = useState({});
+  const [firstRender, setFirstRender] = useState(true);
+
+  const getList = async () => {
+    try {
+      const response = await api.get('/account/13768649/lists');
+      setMeta(response.data);
+    } catch (error) {
+      throw new Error('failed request getList: ' + error);
+    }
+  };
+  const deleteList = id => {
+    api
+      .delete(`/list/${id}?&`)
+      .then(response => console.log(response.data))
+      .catch(err => console.log(err))
+      .finally(() => setListUpdate(Math.random()));
+  };
+
+  useEffect(() => {
+    getList();
+  }, []);
+
+  useEffect(() => {
+    if (!firstRender) {
+      getList();
+    }
+    setFirstRender(false);
+  }, [listUpdate]);
+
   return (
-    <SafeAreaView
-      style={{backgroundColor: 'black', flex: 1, alignItems: 'center'}}>
-      <BtnGoBack
-        style={{
-          alignSelf: 'flex-start',
-          marginLeft: 20,
-          marginTop: 17,
-          marginBottom: 35,
-        }}
-      />
-      <TextBold style={{color: 'white', fontSize: 20, lineHeight: 27}}>
-        Minhas listas
-      </TextBold>
-    </SafeAreaView>
+    <>
+      <CreateList modalActive={modalActive} setModalActive={setModalActive} />
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          ListHeaderComponent={
+            <TextBold style={styles.title}>Minhas listas</TextBold>
+          }
+          style={{width: '100%'}}
+          data={meta?.results}
+          keyExtractor={(_, index) => index}
+          renderItem={({item}) => {
+            return (
+              <View style={styles.card}>
+                <TouchableOpacity style={styles.btnList} activeOpacity={0.8}>
+                  <TextRegular style={styles.listTitle}>
+                    {item?.name}
+                  </TextRegular>
+                  <TextBold style={styles.totalFilms}>
+                    {item?.item_count} Filmes
+                  </TextBold>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  onPress={() => {
+                    deleteList(item?.id);
+                  }}
+                  activeOpacity={0.8}
+                  style={styles.btnDelete}>
+                  <Icon
+                    name="delete"
+                    size={20}
+                    color="rgba(236, 38, 38, 0.61)"
+                  />
+                </TouchableOpacity>
+              </View>
+            );
+          }}
+        />
+        <BtnGoBack style={styles.goBack} />
+        <TouchableOpacity
+          onPress={() => setModalActive(!modalActive)}
+          activeOpacity={0.7}
+          style={{
+            width: 51,
+            height: 51,
+            backgroundColor: '#E9A6A6',
+            borderRadius: 25,
+            alignItems: 'center',
+            justifyContent: 'center',
+            position: 'absolute',
+            right: 42,
+            bottom: 30,
+            elevation: 10,
+          }}>
+          <Icon name="plus" color="black" size={45} />
+        </TouchableOpacity>
+      </SafeAreaView>
+    </>
   );
 }
